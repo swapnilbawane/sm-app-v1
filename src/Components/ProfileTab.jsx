3d234ba4689c5ab649aa7bb014d13a33181fcf5f
Swapnil Bawane <swapnilbawane08@gmail.com>
2023-06-17 00:52:53 +0530

    left aside panel logged user details added

--- src/Components/ProfileTab.jsx
+++ src/Components/ProfileTab.jsx
@@ -1,4 +1,10 @@
+import { useAuth } from "../Context/auth-context";
+
+
 export function ProfileTab() {
+
+    const { currentUser } = useAuth(); 
+    
     return(
         <> 
         <div className="flex flex-space-between flex-align-center">
@@ -6,8 +12,8 @@ export function ProfileTab() {
 <div className="flex">
   <div className="grey-bg br-full width-xl height-xl"></div>
    <div className="flex flex-column ml-xs">
-    <div className="fw-bold">Tanay Pratap</div>
-    <div className="fw-light grey-color">@tanaypratap</div>
+    <div className="fw-bold">{currentUser?.firstName} {currentUser?.lastName}</div>
+    <div className="fw-light grey-color">@{currentUser?.username}</div>
    </div>
 </div>
 <div className="grey-color fw-bold">...</div>
