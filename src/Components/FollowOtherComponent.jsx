e7ecccf3a25d682c179f58317c0c98a0f8dba8d8
Swapnil Bawane <swapnilbawane08@gmail.com>
2023-06-17 01:10:05 +0530

    feat: right aside section follow people list added

--- src/Components/FollowOtherComponent.jsx
+++ src/Components/FollowOtherComponent.jsx
@@ -1,12 +1,19 @@
-export function FollowOtherComponent() { 
+export function FollowOtherComponent({
+_id,
+firstName,
+lastName,
+username  
+}) { 
+
+
     return(
         <> 
         <div className="flex p-s flex-space-between flex-align-center">
         <div className="grey-bg br-full width-xl height-xl"></div>
         <div className="flex flex-column">
           <a href="profile1.html">
-            <div className="fw-bold">Tanay Pratap</div>
-            <div className="fw-light grey-color">@tanaypratap</div>
+            <div className="fw-bold">{firstName} {lastName}</div>
+            <div className="fw-light grey-color">@{username}</div>
           </a>
         </div>
         <div className="primary-color fw-bold">
