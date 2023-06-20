0a323fed1c219d36e237044522fb864ec5f19570
Swapnil Bawane <swapnilbawane08@gmail.com>
2023-06-21 02:13:21 +0530

    todo added

--- src/Pages/Bookmark/MainBookmark.jsx
+++ src/Pages/Bookmark/MainBookmark.jsx
@@ -3,8 +3,15 @@ import '../../index.css'
 import { useAuth } from '../../Context/auth-context'
 import { PostComponent } from '../../Components/PostComponent'
 
+// TODO: 
+// In add to bookmark when I receive bookmark array I update it to current user {...currentUser, bookmark: bookmarkData}
+// Then here I use that array, iterate over currentUser.bookmark and for each it find the data from the data.posts.map because all new and old posts are store in here -- authcontext  
+// for each bookmark id === item._id 
+
 export function MainBookmark() {
     const { data, currentUser, setData } = useAuth()
+
+    console.log("currentuser from main bookmark", currentUser);
 
     return (
         <>
