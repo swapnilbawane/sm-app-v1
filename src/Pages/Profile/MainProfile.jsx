4dc5c101d697aa3084df741d1781355a27d9193c
Swapnil Bawane <swapnilbawane08@gmail.com>
2023-06-20 23:25:18 +0530

    feat: profile part completed

--- src/Pages/Profile/MainProfile.jsx
+++ src/Pages/Profile/MainProfile.jsx
@@ -1,20 +1,20 @@
 import '../../style.css'
 import '../../index.css'
 
-import { PostComponent } from "../../Components/PostComponent"; 
+import { PostComponent } from '../../Components/PostComponent'
 import { useAuth } from '../../Context/auth-context'
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
 export function MainProfile() {
-    const { data, currentUser, setData } = useAuth()
+    const { data, currentUser, setData, loggedUserName } = useAuth()
 
     return (
         <>
             <main className="p-s">
                 <div className="flex flex-column flex-center">
                     <div className="lynx-gray-bg width-7 height-7 br-full"></div>
-                    <h3 className="pt-s">Tanay Pratap</h3>
-                    <p className="grey-color txt-s">@tanaypratap</p>
+                    <h3 className="pt-s">{currentUser.firstName} {currentUser.lastName}</h3>
+                    <p className="grey-color txt-s">@{currentUser.username}</p>
                     <button className="border lynx-white-bg p-xs m-xs fw-semibold width-8">
                         Edit Profile
                     </button>
@@ -23,8 +23,17 @@ export function MainProfile() {
                         biggest programming community | Tweets about JavaScript,
                         ReactJS, Career and Startups
                     </p>
-                    <p className="primary-color"> <a href="https://www.tanaypratap.com" target="_blank" rel="noopener noreferrer">tanaypratap.com </a> </p>
-                    
+                    <p className="primary-color">
+                        {' '}
+                        <a
+                            href="https://www.tanaypratap.com"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            tanaypratap.com{' '}
+                        </a>{' '}
+                    </p>
+
                     <div className="white-bg p-xs m-xs flex flex-row flex-space-evenly">
                         <div className="flex flex-column flex-center m-s ml-m mr-m">
                             <p className="fw-black">0</p>
@@ -48,12 +57,17 @@ export function MainProfile() {
                         {/* TODO: Add a map function here but first get data on first load in login context */}
 
                         {data?.posts?.map((item) => {
-                            const postData = { ...item, ...currentUser }
-                            return (
+                            // const postData = { ...item}
+                            const firstName = currentUser.firstName
+                            const lastName = currentUser.lastName
+
+                            const itemData = {...item, firstName, lastName }
+
+                            return item.username === loggedUserName ? (
                                 <div key={item._id}>
-                                    <PostComponent {...postData} />
+                                    <PostComponent {...itemData } />
                                 </div>
-                            )
+                            ) : null
                         })}
                     </div>
                 </div>
